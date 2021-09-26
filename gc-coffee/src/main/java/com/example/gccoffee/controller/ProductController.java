bdaf759da2aa4ab7265116dda93cbc601109a546
suhyunsim <cnc1106@naver.com>
2021-09-27 03:04:04 +0900

    feat: 타임리프 활용해서 ProductController 구현

---
+++ gc-coffee/src/main/java/com/example/gccoffee/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.example.gccoffee.controller;
+
+import com.example.gccoffee.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String productsPage(Model model) {
+        var products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "product-list";
+    }
+
+    @GetMapping("/new-product")
+    public String newProductPage() {
+        return "new-product";
+    }
+
+    @PostMapping("/products")
+    public String newProduct(CreateProductRequest createProductRequest) {
+        productService.createProduct(
+                createProductRequest.productName(),
+                createProductRequest.category(),
+                createProductRequest.price(),
+                createProductRequest.description());
+        return "redirect:/products";
+    }
+}
